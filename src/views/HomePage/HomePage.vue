b16b816660da777a503d846fd6d26f6e16fec6ae
Shreesh Katyayan <shreesh.blogwebs@gmail.com>
2023-05-26 12:02:21 +0530

    Update HomePage.vue - refractor

--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -88,12 +88,3 @@ const submit = () => {
   );
 };
 </script>
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
